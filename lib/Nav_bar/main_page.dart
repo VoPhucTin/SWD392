cafa39136f3cb06bfa91d7a20afa6846ff78390c
TuanPA0212 <phananhtuan439@gmail.com>
2023-02-07 01:14:13 +0700

    footer

---
+++ lib/Nav_bar/main_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:swd_project/Nav_bar/club_page.dart';
+import 'package:swd_project/Nav_bar/home_page.dart';
+import 'package:swd_project/Nav_bar/post_page.dart';
+import 'package:swd_project/Nav_bar/profile_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [HomePage(), ClubPage(), PostPage(), ProfilePage()];
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          // backgroundColor: Colors.blue,
+          selectedItemColor: Colors.black87,
+          unselectedItemColor: Colors.black38,
+          currentIndex: currentIndex,
+          onTap: (index) => setState(() => currentIndex = index),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+                backgroundColor: Colors.blue),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications),
+                label: 'Club',
+                backgroundColor: Colors.red),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.article),
+                label: 'Post',
+                backgroundColor: Colors.green),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+                backgroundColor: Colors.pink),
+          ]),
+    );
+  }
+}
