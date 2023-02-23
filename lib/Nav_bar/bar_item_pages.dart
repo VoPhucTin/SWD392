63bacbbbde284eb80f426ff178def1a90cf6b611
TuanPA0212 <phananhtuan439@gmail.com>
2023-02-23 14:59:39 +0700

    fix notification

--- lib/Nav_bar/bar_item_pages.dart
+++ lib/Nav_bar/bar_item_pages.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/widgets/placeholder.dart';
 
-class BarItemPage extends StatelessWidget {
-  const BarItemPage({super.key});
+// class BarItemPage extends StatelessWidget {
+//   const BarItemPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("bar item Page"),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Center(
+//         child: Text("bar item Page"),
+//       ),
+//     );
+//   }
+// }
--- lib/Nav_bar/main_page.dart
+++ lib/Nav_bar/main_page.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:swd_project/Nav_bar/club_page.dart';
 import 'package:swd_project/Nav_bar/home_page.dart';
 import 'package:swd_project/Nav_bar/post_page.dart';
@@ -14,6 +18,98 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  String? mtoken = "";
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    requestPermission();
+    getDeviceToken();
+    initInfor();
+  }
+
+  initInfor() {
+    var androidInitialize =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    // var iosInitialize = const IOSInitializationSettings();
+    var initializationSettings =
+        InitializationSettings(android: androidInitialize);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings
+        //     onSelectNotification: (String? payload) async {
+        //   try {
+        //     if (payload != null && payload.isNotEmpty) {
+        //     } else {}
+        //   } catch (e) {}
+        //   return;
+        // }
+        );
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      print("Message: ${message.notification?.title}");
+      print("Message body: ${message.notification?.body}");
+      BigTextStyleInformation bigTextStyleInformation = BigTextStyleInformation(
+        message.notification!.body.toString(),
+        htmlFormatBigText: true,
+        contentTitle: message.notification!.title.toString(),
+        htmlFormatContentTitle: true,
+      );
+      AndroidNotificationDetails androidPlatformChannelSpecifics =
+          AndroidNotificationDetails(
+        'dbfood',
+        'dbfood',
+        importance: Importance.high,
+        styleInformation: bigTextStyleInformation,
+        priority: Priority.high,
+        playSound: true,
+      );
+      NotificationDetails platformChannelSpecifics =
+          NotificationDetails(android: androidPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.show(0, message.notification?.title,
+          message.notification?.body, platformChannelSpecifics,
+          payload: message.data['body']);
+    });
+  }
+
+  void getDeviceToken() async {
+    await FirebaseMessaging.instance.getToken().then((token) {
+      setState(() {
+        mtoken = token;
+        print("My device token is $mtoken");
+      });
+      saveDeviceToken(token!);
+    });
+  }
+
+  void saveDeviceToken(String token) async {
+    await FirebaseFirestore.instance.collection("UserTokens").doc("User2").set({
+      'token': token,
+    });
+  }
+
+  void requestPermission() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print("User granted permission");
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print("User granted provisional permission");
+    } else {
+      print("User declined or has not accepted permission");
+    }
+  }
+
+// Nav-bar here
   List pages = [HomePage(), ClubPage(), PostPage(), ProfilePage()];
   int currentIndex = 0;
 
