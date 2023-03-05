3e77d83f4e7b5f9e39c41ec2daed4655f985f415
VoPhucTin <kimtintin2@gmail.com>
2023-03-05 11:12:54 +0700

    api login

--- lib/services/firebase_services.dart
+++ lib/services/firebase_services.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
 class FirebaseServices {
   final _auth = FirebaseAuth.instance;
@@ -16,16 +19,34 @@ class FirebaseServices {
             accessToken: googleSignInAuthentication.accessToken,
             idToken: googleSignInAuthentication.idToken);
         await _auth.signInWithCredential(authCredential);
-
-        // if(_auth != null){
-        //  String token = _auth.get
-        // }
+        FirebaseAuth fAuth = FirebaseAuth.instance;
+        final User? firebaseUser = (await fAuth
+                .signInWithCredential(authCredential)
+                .catchError((msg) {}))
+            .user;
+        if (firebaseUser != null) {
+          String token = await firebaseUser.getIdToken();
+          print('Get token from firebase: $token');
+          await sendTokenToApi(token);
+        }
         print(googleSignInAuthentication.idToken);
       }
     } on FirebaseAuthException catch (e) {
       print(e.message);
       throw e;
     }
+  }
+
+  Future<void> sendTokenToApi(String token) async {
+    final url = "https://event-project.herokuapp.com/api/login";
+    final headers = {
+      'Content-Type': 'application/json',
+    };
+    final body = json.encode({'token': token, 'role': 'member'});
+    final response =
+        await http.post(Uri.parse(url), headers: headers, body: body);
+    final responseData = json.decode(response.body);
+    print('response data: $responseData');
   }
 
   signOut() async {
